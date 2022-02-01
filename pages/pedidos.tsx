9eae3a019ebef4f248a482d56562983ac2299510
Miguel_EspinalesEs <mee@topfloormarketing.net>
2022-01-31 21:16:13 -0600

    creando rutas de la pagina

---
+++ pages/pedidos.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+
+// componets
+import Layout from '../components/Layout'
+
+const Pedidos: NextPage = () => {
+  return (
+    <>
+        <h1 className='text-2xl font-light text-gray-800'>Pedidos</h1>
+    </>
+  )
+}
+
+export default Pedidos
