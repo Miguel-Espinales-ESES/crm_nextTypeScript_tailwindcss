134f799f73ceca33f6fb581e975be60ef953a358
Miguel_EspinalesEs <mee@topfloormarketing.net>
2022-02-10 13:26:16 -0600

    validando tipo de input y error del input

--- components/InputItem/Types/index.ts
+++ components/InputItem/Types/index.ts
@@ -1,19 +1,28 @@
 export type Props = {
-    label? : string,
-    type : TypeInput,
-    placeholder? : string,
+    label?: string,
+    type: TypeInput,
+    placeholder?: string,
     disabled?: boolean,
     className?: string,
-    validateStatus? : validateStatus
+    validateStatus?: validateStatus
+    errorInput?: {
+        active: boolean,
+        error: string
+    }
 }
 
 export enum TypeInput {
     Text = 'text',
-    Email ='email'
+    Email = 'email'
 }
 
 
 export enum validateStatus {
+    input_secondary = 'input-secondary',
+    input_accent = 'input-accent',
     input_info = 'input-info',
-    input_success = 'input-success'
+    input_success = 'input-success',
+    input_primary = 'input-primary',
+    input_warning = 'input-warning',
+    input_error = 'input-error'
 }
--- components/InputItem/index.tsx
+++ components/InputItem/index.tsx
@@ -1,19 +1,36 @@
 import React from 'react'
-import { Props , TypeInput, validateStatus } from './Types'
+// type props
+import { Props, TypeInput, validateStatus } from './Types'
 import { isUndefined } from 'lodash'
 
-const InputItem: React.FC<Props> = ({ type, label, disabled, placeholder, className: classNameInput , validateStatus }) => {
+const InputItem: React.FC<Props> = ({ type, label, disabled, placeholder, className: classNameInput, validateStatus: validateStatusClass, errorInput }) => {
     return (
         <>
             <label className="label">
                 <span className="label-text">{label}</span>
             </label >
-            <input
-                disabled={(isUndefined(disabled)) ? disabled : false}
-                type={type}
-                placeholder={placeholder}
-                className={`input input-bordered ${classNameInput} ${validateStatus}`}>
-            </input>
+            {
+                (errorInput?.active) ? (
+                    <>
+                        <input
+                            disabled={(isUndefined(disabled)) ? disabled : false}
+                            type={type}
+                            placeholder={placeholder}
+                            className={`input input-bordered ${classNameInput} ${validateStatus.input_error}`}>
+                        </input>
+                        <label className="label">
+                            <span className="label-text-alt text-accent">{errorInput.error}</span>
+                        </label>
+                    </>
+                ) : (
+                    <input
+                        disabled={(isUndefined(disabled)) ? disabled : false}
+                        type={type}
+                        placeholder={placeholder}
+                        className={`input input-bordered ${classNameInput} ${validateStatusClass}`}>
+                    </input>
+                )
+            }
         </>
     )
 }
