84de5d49e7da09fc56cc1ecb67a4d9ccf4de60d7
Miguel_EspinalesEs <mee@topfloormarketing.net>
2022-02-16 17:47:53 -0600

    agregando alertas con react toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,21 @@
+// css
 import '../styles/globals.css'
+import 'react-toastify/dist/ReactToastify.css';
+
 import type { AppProps } from 'next/app'
 // apollo
 import { ApolloProvider } from '@apollo/client'
 import client from '../config/apollo'
-
 // componets
 import Layout from '../components/Layout'
+// alert
+import { ToastContainer } from 'react-toastify';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div data-theme="emerald">
       <ApolloProvider client={client}>
+        <ToastContainer />
         <Layout>
           <Component  {...pageProps} />
         </Layout>
